f8ed07a38ef73304b647e8287d7b83cea1f5e7cf
Austin Papritz <austin@papritz.dev>
2023-09-08 13:25:32 -0700

    added bowlPosition state to Grid.js

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Grid.css';
 
 function Grid() {
+const [bowlPosition, setBowlPosition] = useState(1);
   return (
     <div className="grid">
       {/* TOP ROW (cereal boxes) */}
@@ -15,9 +16,9 @@ function Grid() {
       <div className="cell"></div>
 
       {/* BOTTOM ROW (user bowl) */}
-      <div className="cell"></div>
-      <div className="cell active"></div>
-      <div className="cell"></div>
+      <div className={bowlPosition === 0 ? 'cell active' : 'cell'}></div>
+      <div className={bowlPosition === 1 ? 'cell active' : 'cell'}></div>
+      <div className={bowlPosition === 2 ? 'cell active' : 'cell'}></div>
     </div>
   );
 }

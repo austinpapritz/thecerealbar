5d3fbf1373b00fda6c6cd52a4b3fb80be156ca01
Austin Papritz <austin@papritz.dev>
2023-09-11 08:09:56 -0700

    colors fill bowl but colors and bowlPosition are bugged

--- src/components/Grid/Bottom/UserBowl.js
+++ src/components/Grid/Bottom/UserBowl.js
@@ -2,19 +2,26 @@ import './UserBowl.css';
 
 const UserBowl = ({ bowl, className }) => {
     const buildBowlBackground = () => {
-      let gradients = '';
-      let start = 0;
-  
-      bowl.forEach(cereal => {
-        const end = start + cereal.amount;
-        gradients += `, ${cereal.color} ${start}%, ${cereal.color} ${end}%, transparent ${end}%`;
-        start = end;
-      });
-  
-      return `linear-gradient(to top${gradients})`;
+        let gradients = [];
+        let start = 0;
+
+        bowl.forEach(cereal => {
+            const end = start + cereal.amount;
+            if (cereal.amount > 0) {
+                gradients.push(`${cereal.cereal} ${start}%, ${cereal.cereal} ${end}%`);
+            }
+            start = end;
+        });
+
+        if (gradients.length === 0) {
+            return "transparent";
+        }
+
+        return `linear-gradient(to top, ${gradients.join(", ")})`;
     };
-  
+
+
     return <div className={`bowl ${className}`} style={{ background: buildBowlBackground() }}></div>;
-  };
+};
 
 export default UserBowl;
--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,95 +1,94 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import UserBowl from './Bottom/UserBowl.js';
 import CerealBox from './Top/CerealBox.js';
 import './Grid.css';
 
 function Grid() {
-const [bowlPosition, setBowlPosition] = useState(1);
-const [cerealBoxes, setCerealBoxes] = useState([
-    { cereal: 'blue', amount: 100 },
-    { cereal: 'red', amount: 100 },
-    { cereal: 'green', amount: 100 },
+    const [bowlPosition, setBowlPosition] = useState(1);
+    const [cerealBoxes, setCerealBoxes] = useState([
+        { cereal: 'blue', amount: 100 },
+        { cereal: 'red', amount: 100 },
+        { cereal: 'green', amount: 100 },
     ]);
-const [bowl, setBowl] = useState([
-    { cereal: 'blue', amount: 0 },
-    { cereal: 'red', amount: 0 },
-    { cereal: 'green', amount: 0 },
+    const [bowl, setBowl] = useState([
+        { cereal: 'blue', amount: 0 },
+        { cereal: 'red', amount: 0 },
+        { cereal: 'green', amount: 0 },
     ]);
 
 
-useEffect(() => {
-    const handleKeyPress = (e) => {
-      if (e.key === 'ArrowLeft' && bowlPosition > 0) {
-        setBowlPosition(bowlPosition - 1);
-      } else if (e.key === 'ArrowRight' && bowlPosition < 2) {
-        setBowlPosition(bowlPosition + 1);
-      } else if (e.key === 'ArrowDown') {
-        transferCereal();
-      }
-    };
+    useEffect(() => {
+        const handleKeyPress = (e) => {
+            if (e.key === 'ArrowLeft' && bowlPosition > 0) {
+                setBowlPosition(bowlPosition - 1);
+            } else if (e.key === 'ArrowRight' && bowlPosition < 2) {
+                setBowlPosition(bowlPosition + 1);
+            } else if (e.key === 'ArrowDown') {
+                transferCereal();
+            }
+        };
 
-    window.addEventListener('keydown', handleKeyPress);
+        window.addEventListener('keydown', handleKeyPress);
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress);
-    };
-  }, [bowlPosition, cerealBoxes, bowl]);
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [bowlPosition, cerealBoxes, bowl]);
 
-  // On down arrow press
-const transferCereal = () => {
-    // Get the cereal box above the current bowl position
-    const currentBox = cerealBoxes[bowlPosition];
-  
-    const transferRate = 10;
-  
-    // Check if the bowl already has this type of cereal
-    const existingCerealInBowl = bowl.find(c => c.color === currentBox.color);
-  
-    // Transfer cereal from box to bowl
-    if (currentBox.amount >= transferRate) {
-      // Update the cereal box amount
-      setCerealBoxes(prevState => prevState.map((box, index) => {
-        if (index === bowlPosition) {
-          return { ...box, amount: box.amount - transferRate };
+    // On down arrow press
+    const transferCereal = () => {
+        // Get the cereal box above the current bowl position
+        const currentBox = cerealBoxes[bowlPosition];
+
+        const transferRate = 10;
+
+        // Check if the bowl already has this type of cereal
+        const existingCerealInBowl = bowl.find(c => c.color === currentBox.color);
+
+        // Transfer cereal from box to bowl
+        if (currentBox.amount >= transferRate) {
+            // Update the cereal box amount
+            setCerealBoxes(prevState => prevState.map((box, index) => {
+                if (index === bowlPosition) {
+                    return { ...box, amount: box.amount - transferRate };
+                }
+                return box;
+            }));
+
+            // Update the bowl
+            if (existingCerealInBowl) {
+                // Add to the existing amount in the bowl, if there
+                setBowl(prevState => prevState.map(c => {
+                    if (c.color === currentBox.color) {
+                        return { ...c, amount: c.amount + transferRate };
+                    }
+                    return c;
+                }));
+            } else {
+                // Add new cereal type to the bowl if not already there
+                setBowl(prevState => [...prevState, { color: currentBox.color, amount: transferRate }]);
+            }
         }
-        return box;
-      }));
-  
-      // Update the bowl
-      if (existingCerealInBowl) {
-        // Add to the existing amount in the bowl, if there
-        setBowl(prevState => prevState.map(c => {
-          if (c.color === currentBox.color) {
-            return { ...c, amount: c.amount + transferRate };
-          }
-          return c;
-        }));
-      } else {
-        // Add new cereal type to the bowl if not already there
-        setBowl(prevState => [...prevState, { color: currentBox.color, amount: transferRate }]);
-      }
-    }
-  };
-  
-  return (
-    <div className="grid">
-        {/* TOP ROW (cereal boxes) */}
-        {cerealBoxes.map((box) => {
-           return <CerealBox color={box.cereal} amount={box.amount} />
-        })}
+    };
 
-        {/* MIDDLE ROW (arrow indicator) */}
-        <div className={bowlPosition === 0 ? 'cell indicator' : 'cell'}></div>
-        <div className={bowlPosition === 1 ? 'cell indicator' : 'cell'}></div>
-        <div className={bowlPosition === 2 ? 'cell indicator' : 'cell'}></div>
+    return (
+        <div className="grid">
+            {/* TOP ROW (cereal boxes) */}
+            {cerealBoxes.map((box, index) => {
+                return <CerealBox key={index} color={box.cereal} amount={box.amount} />
+            })}
 
-        {/* BOTTOM ROW (user bowl) */}
-        <UserBowl className={bowlPosition === 0 ? 'active' : ''} bowl={bowl} />
-        <UserBowl className={bowlPosition === 1 ? 'active' : ''} bowl={bowl} />
-        <UserBowl className={bowlPosition === 2 ? 'active' : ''} bowl={bowl} />
+            {/* MIDDLE ROW (arrow indicator) */}
+            <div className={bowlPosition === 0 ? 'cell indicator' : 'cell'}></div>
+            <div className={bowlPosition === 1 ? 'cell indicator' : 'cell'}></div>
+            <div className={bowlPosition === 2 ? 'cell indicator' : 'cell'}></div>
 
-    </div>
-  );
+            {/* BOTTOM ROW (user bowl) */}
+            <UserBowl className="bowl" bowl={bowl} style={{ gridColumnStart: bowlPosition, gridRowStart: 3}}/>
+
+
+        </div>
+    );
 }
 
 export default Grid;
